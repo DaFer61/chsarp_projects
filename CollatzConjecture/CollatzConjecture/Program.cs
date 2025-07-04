b4edfeb98666988bfa8f435594f18f297b978558
Dávid <fertaly.david@gmail.com>
2025-07-04 15:37:47 +0200

    Add files via upload

---
+++ CollatzConjecture/CollatzConjecture/Program.cs
@@ -0,0 +1,26 @@
+﻿//collatz conjecture
+int n = 1;
+while (true)
+{
+    int conj = n;
+    int iter = 0;
+    while (conj > 1)
+    {
+        if (conj % 2 == 0)
+        {
+            conj /= 2;
+        }
+        else
+        {
+            conj = conj * 3 + 1;
+        }
+        iter++;
+    }
+
+    if(iter > 500)
+    {
+        Console.WriteLine($"{n}- number took {iter} iterations.");
+    }
+    n++;
+}
+ 
